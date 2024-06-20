9fc6eb31cc8e6e1f0aed424a14632dfa948e7945
RafaelKrewer <rafaelkrewer5@gmail.com>
2024-06-20 17:20:32 -0300

    commit inicial

---
+++ jogo.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+pos_x=640
+pos_y=320
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        if event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:  # Verifica se a tecla para cima foi pressionada
+                pos_y -= 10  # Move a posição y para cima
+        
+
+    screen.fill("purple")
+    pygame.draw.circle(screen,"green",(pos_x,pos_y),5)
+    pygame.display.flip()
+
+    clock.tick(60)  
+
+
+pygame.quit()
