b763517dfd06af1b3d19cc9681ab2a59f4876178
RafaelKrewer <rafaelkrewer5@gmail.com>
2024-06-21 09:46:14 -0300

    setas de baixo, esquerda e direita

--- jogo.py
+++ jogo.py
@@ -19,6 +19,20 @@ while running:
             if keys[pygame.K_UP]:  # Verifica se a tecla para cima foi pressionada
                 pos_y -= 10  # Move a posição y para cima
         
+        if event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_DOWN]:
+                pos_y += 10
+
+        if event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_RIGHT]:
+                pos_x += 10
+
+        if event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                pos_x -= 10
 
     screen.fill("purple")
     pygame.draw.circle(screen,"green",(pos_x,pos_y),5)
